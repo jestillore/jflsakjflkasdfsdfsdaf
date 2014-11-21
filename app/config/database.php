0b7e21798a3c794fc5d9a9d36c54771e2843c7ab
Jillberth Estillore <ejillberth@gmail.com>
2014-11-21 10:15:55 +0800

    Set database config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'zoogtech_golfapp',
+			'username'  => 'zoogtech_golfapp',
+			'password'  => 'z00gtech101',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
