62f46dda21601412a90cb44d5fd7e5e8a25d581b
Arnie <alavares@zoogtech.com>
2014-12-26 00:30:43 +0800

    Relate member_id to Users

--- app/models/ClosedCompetitionCompetitor.php
+++ app/models/ClosedCompetitionCompetitor.php
@@ -5,5 +5,13 @@ class ClosedCompetitionCompetitor extends BaseModel {
 	protected $table = 'closed_competition_competitors';
 	public $timestamps = false;
 
+	public static = [
+		'member' => [self::BELONGS_TO, 'User','foreignKey' => 'member_id']
+	];
+
+	public function toArray(){
+		$this->load('member');
+		return parent::toArray();
+	}
 
 }
