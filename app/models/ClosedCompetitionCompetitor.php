d1b3d2762e2109856ba0ff57f5a1a41477ff00b4
Arnie <alavares@zoogtech.com>
2014-12-25 23:25:44 +0800

    Add view list of Competitors in a group

--- app/models/ClosedCompetitionCompetitor.php
+++ app/models/ClosedCompetitionCompetitor.php
@@ -5,4 +5,13 @@ class ClosedCompetitionCompetitor extends BaseModel {
 	protected $table = 'closed_competition_competitors';
 	public $timestamps = false;
 
+	public static $relationsData = [
+		'competitors' => [self::HAS_MANY,'ClosedCompetitionCompetitor','foreignKey'=>'closed_competition_group_id']
+	];
+
+	public function toArray(){
+		$this->load('competitors');
+		return parent::toArray();
+	}
+
 }
