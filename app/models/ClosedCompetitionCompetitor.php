df940c469aab474d79a87de75eef60b77fabc6bf
Arnie <alavares@zoogtech.com>
2014-12-25 23:50:48 +0800

    Change content of Competitor to Group

--- app/models/ClosedCompetitionCompetitor.php
+++ app/models/ClosedCompetitionCompetitor.php
@@ -5,13 +5,5 @@ class ClosedCompetitionCompetitor extends BaseModel {
 	protected $table = 'closed_competition_competitors';
 	public $timestamps = false;
 
-	public static $relationsData = [
-		'competitors' => [self::HAS_MANY,'ClosedCompetitionCompetitor','foreignKey'=>'closed_competition_group_id']
-	];
-
-	public function toArray(){
-		$this->load('competitors');
-		return parent::toArray();
-	}
 
 }
--- app/models/ClosedCompetitionGroup.php
+++ app/models/ClosedCompetitionGroup.php
@@ -5,4 +5,12 @@ class ClosedCompetitionGroup extends BaseModel {
 	protected $table = 'closed_competition_groups';
 	public $timestamps = false;
 
+	public static $relationsData = [
+		'competitors' => [self::HAS_MANY,'ClosedCompetitionCompetitor','foreignKey'=>'closed_competition_group_id']
+	];
+
+	public function toArray(){
+		$this->load('competitors');
+		return parent::toArray();
+	}
 }
