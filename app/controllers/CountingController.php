cc624bce025d5744022c3b7f5d541e590e2fb4fc
Jillberth Estillore <jestillore@zoogtech.com>
2015-02-10 17:58:49 +0800

    Closed competition counting by competition

--- app/controllers/CountingController.php
+++ app/controllers/CountingController.php
@@ -130,27 +130,83 @@ class CountingController extends BaseController {
 	// 	return $res;
 	// }
 
-	public function getClosedCompetition($id) { // course id
+	// public function getClosedCompetition($id) { // course id
+	// 	$data = [];
+	// 	$res = [];
+	// 	/**
+	// 	* get all groups by course
+	// 	*/
+	// 	// get all closed competition
+	// 	$cc = ClosedCompetition::where('course_id', '=', $id)->get();
+	// 	foreach($cc as $c) {
+	// 		foreach($c->groups as $group) {
+	// 			$scores = ClosedCompetitionScore::where('closed_competition_id', '=', $c->id)
+	// 				->where('closed_competition_group_id', '=', $group->id)
+	// 				->get();
+	// 			foreach($scores as $score) {
+	// 				if(!isset($data[$score->closed_competition_competitor_id])) {
+	// 					$data[$score->closed_competition_competitor_id] = [];
+	// 				}
+	// 				$data[$score->closed_competition_competitor_id][] = $score;
+	// 			}
+	// 		}
+	// 	}
+
+	// 	// format response
+	// 	foreach($data as $key => $s) {
+	// 		$ccc = ClosedCompetitionCompetitor::find($key);
+	// 		$gross = 0;
+	// 		$net = 0;
+	// 		$handicap = 0;
+	// 		// total scores
+	// 		foreach($s as $score) {
+	// 			$handicap = $score->handicap;
+	// 			$gross += ($score->score/* - $score->hole->par*/);
+	// 		}
+	// 		$net = $gross - $handicap;
+	// 		$res[] = [
+	// 			//'name' => $ccc->member->firstname . ' ' . $ccc->member->lastname,
+	// 			'member' => $ccc->member,
+	// 			'gross' => $gross,
+	// 			'net' => $net
+	// 		];
+	// 	}
+	// 	// sort by net value
+	// 	usort($res, function ($a, $b) {
+	// 		return $a['net'] - $b['net'];
+	// 	});
+	// 	/**
+	// 	* add rank base on index
+	// 	*/
+	// 	for($x = 0; $x < count($res); $x++) {
+	// 		$res[$x]['rank'] = $x + 1;
+	// 	}
+	// 	return $res;
+	// }
+
+	public function getClosedCompetition($id) { // competition id
+
 		$data = [];
 		$res = [];
 		/**
 		* get all groups by course
 		*/
 		// get all closed competition
-		$cc = ClosedCompetition::where('course_id', '=', $id)->get();
-		foreach($cc as $c) {
-			foreach($c->groups as $group) {
-				$scores = ClosedCompetitionScore::where('closed_competition_id', '=', $c->id)
-					->where('closed_competition_group_id', '=', $group->id)
-					->get();
-				foreach($scores as $score) {
-					if(!isset($data[$score->closed_competition_competitor_id])) {
-						$data[$score->closed_competition_competitor_id] = [];
-					}
-					$data[$score->closed_competition_competitor_id][] = $score;
+		// $cc = ClosedCompetition::where('course_id', '=', $id)->get();
+		// foreach($cc as $c) {
+		$c = ClosedCompetition::find($id);
+		foreach($c->groups as $group) {
+			$scores = ClosedCompetitionScore::where('closed_competition_id', '=', $c->id)
+				->where('closed_competition_group_id', '=', $group->id)
+				->get();
+			foreach($scores as $score) {
+				if(!isset($data[$score->closed_competition_competitor_id])) {
+					$data[$score->closed_competition_competitor_id] = [];
 				}
+				$data[$score->closed_competition_competitor_id][] = $score;
 			}
 		}
+		// }
 
 		// format response
 		foreach($data as $key => $s) {
@@ -182,6 +238,7 @@ class CountingController extends BaseController {
 			$res[$x]['rank'] = $x + 1;
 		}
 		return $res;
+
 	}
 
 }
